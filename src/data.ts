03d0affef72f8435d6d76c0d5bf857259a4f3a5c
Sujit-Bishwokarma <sujitkhati7@gmail.com>
2026-06-07 06:58:16 +0545

    Replace hero image with new asset

--- src/data.ts
+++ src/data.ts
@@ -6,7 +6,7 @@
 import { Room, Testimonial, AmenityItem } from './types';
 
 // Static assets imported so Vite bundles and resolves them correctly in production
-import heroImg from './assets/images/hero_orchid_1780650357754.png';
+import heroImg from './assets/images/IMG_20260606_231903.png';
 import roomDeluxeImg from './assets/images/room_deluxe_1780650380249.png';
 import roomSuiteImg from './assets/images/room_suite_1780650397324.png';
 import restaurantImg from './assets/images/restaurant_orchid_1780650414208.png';
